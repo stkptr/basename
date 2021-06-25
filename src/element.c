aa511364246f28feb299c1d360c304ca20dbd50d
stkptr <85911392+stkptr@users.noreply.github.com>
2021-06-25 12:14:59 -0700

    Clean up element value functions a bit

--- src/element.c
+++ src/element.c
@@ -12,101 +12,104 @@ int element_value_is_preference(enum ELEMENT_VALUE ev) {
 }
 
 
-#define OPTION(final, medial) return (is_final) ? (final) : (medial)
-#define BARE(bare, medial) return (is_bare) ? (bare) : (medial)
+#define OPTION(n, final, medial) case (n): return (is_final) ? (final) : (medial)
+#define BARE(n, bare, medial) case (n): return (is_bare) ? (bare) : (medial)
 
 enum ELEMENT_VALUE base_as_element(int n, int is_final, int is_bare) {
     switch (n) {
-        case 0:  OPTION(ELEMENT_VALUE_NULLARY,      ELEMENT_VALUE_INVALID);
-        case 1:  OPTION(ELEMENT_VALUE_UNARY,        ELEMENT_VALUE_INVALID);
-        case 2:  OPTION(ELEMENT_VALUE_BINARY,       ELEMENT_VALUE_BI);
-        case 3:  OPTION(ELEMENT_VALUE_TRINARY,      ELEMENT_VALUE_TRI);
-        case 4:  OPTION(ELEMENT_VALUE_QUATERNARY,   ELEMENT_VALUE_TETRA);
-        case 5:  OPTION(ELEMENT_VALUE_QUINARY,      ELEMENT_VALUE_PENTA);
-        case 6:  OPTION(ELEMENT_VALUE_SEXIMAL,      ELEMENT_VALUE_HEXA);
-        case 7:  OPTION(ELEMENT_VALUE_SEPTIMAL,     ELEMENT_VALUE_HEPTA);
-        case 8:  OPTION(ELEMENT_VALUE_OCTAL,        ELEMENT_VALUE_OCTO);
-        case 9:  OPTION(ELEMENT_VALUE_NONARY,       ELEMENT_VALUE_ENNA);
-        case 10: BARE  (ELEMENT_VALUE_DECIMAL,      ELEMENT_VALUE_GESIMAL);
-        case 11: OPTION(ELEMENT_VALUE_ELEVENARY,    ELEMENT_VALUE_LEVA);
-        case 12: OPTION(ELEMENT_VALUE_DOZENAL,      ELEMENT_VALUE_DOZA);
-        case 13: BARE  (ELEMENT_VALUE_BAKERS_DOZENAL, ELEMENT_VALUE_KERS_DOZENAL);
+        OPTION(0,   ELEMENT_VALUE_NULLARY,      ELEMENT_VALUE_INVALID);
+        OPTION(1,   ELEMENT_VALUE_UNARY,        ELEMENT_VALUE_INVALID);
+        OPTION(2,   ELEMENT_VALUE_BINARY,       ELEMENT_VALUE_BI);
+        OPTION(3,   ELEMENT_VALUE_TRINARY,      ELEMENT_VALUE_TRI);
+        OPTION(4,   ELEMENT_VALUE_QUATERNARY,   ELEMENT_VALUE_TETRA);
+        OPTION(5,   ELEMENT_VALUE_QUINARY,      ELEMENT_VALUE_PENTA);
+        OPTION(6,   ELEMENT_VALUE_SEXIMAL,      ELEMENT_VALUE_HEXA);
+        OPTION(7,   ELEMENT_VALUE_SEPTIMAL,     ELEMENT_VALUE_HEPTA);
+        OPTION(8,   ELEMENT_VALUE_OCTAL,        ELEMENT_VALUE_OCTO);
+        OPTION(9,   ELEMENT_VALUE_NONARY,       ELEMENT_VALUE_ENNA);
+        BARE  (10,  ELEMENT_VALUE_DECIMAL,      ELEMENT_VALUE_GESIMAL);
+        OPTION(11,  ELEMENT_VALUE_ELEVENARY,    ELEMENT_VALUE_LEVA);
+        OPTION(12,  ELEMENT_VALUE_DOZENAL,      ELEMENT_VALUE_DOZA);
+        BARE  (13,  ELEMENT_VALUE_BAKERS_DOZENAL, ELEMENT_VALUE_KERS_DOZENAL);
         //
-        case 16: OPTION(ELEMENT_VALUE_HEX,          ELEMENT_VALUE_TESSER);
-        case 17: OPTION(ELEMENT_VALUE_SUBOPTIMAL,   ELEMENT_VALUE_MAL);
+        OPTION(16,  ELEMENT_VALUE_HEX,          ELEMENT_VALUE_TESSER);
+        OPTION(17,  ELEMENT_VALUE_SUBOPTIMAL,   ELEMENT_VALUE_MAL);
         //
-        case 20: OPTION(ELEMENT_VALUE_VIGESIMAL,    ELEMENT_VALUE_ICOSI);
+        OPTION(20,  ELEMENT_VALUE_VIGESIMAL,    ELEMENT_VALUE_ICOSI);
         //
-        case 36: OPTION(ELEMENT_VALUE_NIFTIMAL,     ELEMENT_VALUE_FETA);
+        OPTION(36,  ELEMENT_VALUE_NIFTIMAL,     ELEMENT_VALUE_FETA);
         //
-        case 100:OPTION(ELEMENT_VALUE_CENTESIMAL,   ELEMENT_VALUE_HECTO);
+        OPTION(100, ELEMENT_VALUE_CENTESIMAL,   ELEMENT_VALUE_HECTO);
         //
         default: return ELEMENT_VALUE_INVALID;
     }
 }
 
 #undef OPTION
+#undef BARE
 
+
+#define RET(match, value) case (match): return (value)
 
 const char *element_as_string(enum ELEMENT_VALUE ev) {
     switch (ev) {
-        case ELEMENT_VALUE_INVALID: return "invalid";
+        RET(ELEMENT_VALUE_INVALID, "invalid");
         //
-        case ELEMENT_VALUE_NEGA: return "nega";
-        case ELEMENT_VALUE_VOT: return "vot";
+        RET(ELEMENT_VALUE_NEGA, "nega");
+        RET(ELEMENT_VALUE_VOT, "vot");
         //
-        case ELEMENT_VALUE_NULLARY: return "nullary";
-        case ELEMENT_VALUE_UNARY: return "unary";
-            case ELEMENT_VALUE_UN: return "un";
-            case ELEMENT_VALUE_HEN: return "hen";
-            case ELEMENT_VALUE_SNA: return "sna";
-        case ELEMENT_VALUE_BINARY: return "binary";
-            case ELEMENT_VALUE_BI: return "bi";
-        case ELEMENT_VALUE_TRINARY: return "trinary";
-            case ELEMENT_VALUE_TRI: return "tri";
-        case ELEMENT_VALUE_QUATERNARY: return "quaternary";
-            case ELEMENT_VALUE_TETRA: return "tetra";
-        case ELEMENT_VALUE_QUINARY: return "quinary";
-            case ELEMENT_VALUE_PENTA: return "penta";
-        case ELEMENT_VALUE_SEXIMAL: return "seximal";
-            case ELEMENT_VALUE_HEXA: return "hexa";
-        case ELEMENT_VALUE_SEPTIMAL: return "septimal";
-            case ELEMENT_VALUE_HEPTA: return "hepta";
-        case ELEMENT_VALUE_OCTAL: return "octal";
-            case ELEMENT_VALUE_OCTO: return "octo";
-        case ELEMENT_VALUE_NONARY: return "nonary";
-            case ELEMENT_VALUE_ENNA: return "enna";
-        case ELEMENT_VALUE_DECIMAL: return "decimal";
-            case ELEMENT_VALUE_GESIMAL: return "gesimal";
-        case ELEMENT_VALUE_ELEVENARY: return "elevenary";
-            case ELEMENT_VALUE_LEVA: return "leva";
-        case ELEMENT_VALUE_DOZENAL: return "dozenal";
-            case ELEMENT_VALUE_DOZA: return "doza";
-        case ELEMENT_VALUE_BAKERS_DOZENAL: return "baker's dozenal";
-            case ELEMENT_VALUE_KERS_DOZENAL: return "ker's dozenal";
+        RET(ELEMENT_VALUE_NULLARY, "nullary");
+        RET(ELEMENT_VALUE_UNARY, "unary");
+            RET(ELEMENT_VALUE_UN, "un");
+            RET(ELEMENT_VALUE_HEN, "hen");
+            RET(ELEMENT_VALUE_SNA, "sna");
+        RET(ELEMENT_VALUE_BINARY, "binary");
+            RET(ELEMENT_VALUE_BI, "bi");
+        RET(ELEMENT_VALUE_TRINARY, "trinary");
+            RET(ELEMENT_VALUE_TRI, "tri");
+        RET(ELEMENT_VALUE_QUATERNARY, "quaternary");
+            RET(ELEMENT_VALUE_TETRA, "tetra");
+        RET(ELEMENT_VALUE_QUINARY, "quinary");
+            RET(ELEMENT_VALUE_PENTA, "penta");
+        RET(ELEMENT_VALUE_SEXIMAL, "seximal");
+            RET(ELEMENT_VALUE_HEXA, "hexa");
+        RET(ELEMENT_VALUE_SEPTIMAL, "septimal");
+            RET(ELEMENT_VALUE_HEPTA, "hepta");
+        RET(ELEMENT_VALUE_OCTAL, "octal");
+            RET(ELEMENT_VALUE_OCTO, "octo");
+        RET(ELEMENT_VALUE_NONARY, "nonary");
+            RET(ELEMENT_VALUE_ENNA, "enna");
+        RET(ELEMENT_VALUE_DECIMAL, "decimal");
+            RET(ELEMENT_VALUE_GESIMAL, "gesimal");
+        RET(ELEMENT_VALUE_ELEVENARY, "elevenary");
+            RET(ELEMENT_VALUE_LEVA, "leva");
+        RET(ELEMENT_VALUE_DOZENAL, "dozenal");
+            RET(ELEMENT_VALUE_DOZA, "doza");
+        RET(ELEMENT_VALUE_BAKERS_DOZENAL, "baker's dozenal");
+            RET(ELEMENT_VALUE_KERS_DOZENAL, "ker's dozenal");
         //
-        case ELEMENT_VALUE_HEX: return "hex";
-            case ELEMENT_VALUE_TESSER: return "tesser";
-        case ELEMENT_VALUE_SUBOPTIMAL: return "suboptimal";
-            case ELEMENT_VALUE_MAL: return "mal";
+        RET(ELEMENT_VALUE_HEX, "hex");
+            RET(ELEMENT_VALUE_TESSER, "tesser");
+        RET(ELEMENT_VALUE_SUBOPTIMAL, "suboptimal");
+            RET(ELEMENT_VALUE_MAL, "mal");
         //
-        case ELEMENT_VALUE_VIGESIMAL: return "vigesimal";
-            case ELEMENT_VALUE_ICOSI: return "icosi";
+        RET(ELEMENT_VALUE_VIGESIMAL, "vigesimal");
+            RET(ELEMENT_VALUE_ICOSI, "icosi");
         //
-        case ELEMENT_VALUE_NIFTIMAL: return "niftimal";
-            case ELEMENT_VALUE_FETA: return "feta";
+        RET(ELEMENT_VALUE_NIFTIMAL, "niftimal");
+            RET(ELEMENT_VALUE_FETA, "feta");
         //
-        case ELEMENT_VALUE_CENTESIMAL: return "centesimal";
-            case ELEMENT_VALUE_HECTO: return "hecto";
+        RET(ELEMENT_VALUE_CENTESIMAL, "centesimal");
+            RET(ELEMENT_VALUE_HECTO, "hecto");
         //
-        case ELEMENT_VALUE_NARY: return "nary";
-            case ELEMENT_VALUE_ARY: return "ary";
-        case ELEMENT_VALUE_IMAL: return "imal";
-            case ELEMENT_VALUE_AL: return "al";
+        RET(ELEMENT_VALUE_NARY, "nary");
+            RET(ELEMENT_VALUE_ARY, "ary");
+        RET(ELEMENT_VALUE_IMAL, "imal");
+            RET(ELEMENT_VALUE_AL, "al");
         //
-        case ELEMENT_VALUE_PREFER_NEITHER: return "0";
-        case ELEMENT_VALUE_PREFER_FIRST: return "<-";
-        case ELEMENT_VALUE_PREFER_LAST: return "->";
+        RET(ELEMENT_VALUE_PREFER_NEITHER, "0");
+        RET(ELEMENT_VALUE_PREFER_FIRST, "<-");
+        RET(ELEMENT_VALUE_PREFER_LAST, "->");
     }
 
     return "";
@@ -115,62 +118,61 @@ const char *element_as_string(enum ELEMENT_VALUE ev) {
 
 char element_final_char(enum ELEMENT_VALUE ev) {
     switch (ev) {
-        case ELEMENT_VALUE_INVALID: return 'd';
+        RET(ELEMENT_VALUE_INVALID, 'd');
         //
-        case ELEMENT_VALUE_NEGA: return 'a';
-        case ELEMENT_VALUE_VOT: return 't';
+        RET(ELEMENT_VALUE_NEGA, 'a');
+        RET(ELEMENT_VALUE_VOT, 't');
         //
-        case ELEMENT_VALUE_NULLARY: return 'y';
-        case ELEMENT_VALUE_UNARY: return 'y';
-            case ELEMENT_VALUE_UN: return 'n';
-            case ELEMENT_VALUE_HEN: return 'n';
-            case ELEMENT_VALUE_SNA: return 'a';
-        case ELEMENT_VALUE_BINARY: return 'y';
-            case ELEMENT_VALUE_BI: return 'i';
-        case ELEMENT_VALUE_TRINARY: return 'y';
-            case ELEMENT_VALUE_TRI: return 'i';
-        case ELEMENT_VALUE_QUATERNARY: return 'y';
-            case ELEMENT_VALUE_TETRA: return 'a';
-        case ELEMENT_VALUE_QUINARY: return 'y';
-            case ELEMENT_VALUE_PENTA: return 'a';
-        case ELEMENT_VALUE_SEXIMAL: return 'l';
-            case ELEMENT_VALUE_HEXA: return 'a';
-        case ELEMENT_VALUE_SEPTIMAL: return 'l';
-            case ELEMENT_VALUE_HEPTA: return 'a';
-        case ELEMENT_VALUE_OCTAL: return 'l';
-            case ELEMENT_VALUE_OCTO: return 'o';
-        case ELEMENT_VALUE_NONARY: return 'y';
-            case ELEMENT_VALUE_ENNA: return 'a';
-        case ELEMENT_VALUE_DECIMAL: return 'l';
-            case ELEMENT_VALUE_GESIMAL: return 'l';
-        case ELEMENT_VALUE_ELEVENARY: return 'y';
-            case ELEMENT_VALUE_LEVA: return 'a';
-        case ELEMENT_VALUE_DOZENAL: return 'l';
-            case ELEMENT_VALUE_DOZA: return 'a';
-        case ELEMENT_VALUE_BAKERS_DOZENAL: return 'l';
-            case ELEMENT_VALUE_KERS_DOZENAL: return 'l';
+        RET(ELEMENT_VALUE_NULLARY, 'y');
+        RET(ELEMENT_VALUE_UNARY, 'y');
+            RET(ELEMENT_VALUE_UN, 'n');
+            RET(ELEMENT_VALUE_HEN, 'n');
+            RET(ELEMENT_VALUE_SNA, 'a');
+        RET(ELEMENT_VALUE_BINARY, 'y');
+            RET(ELEMENT_VALUE_BI, 'i');
+        RET(ELEMENT_VALUE_TRINARY, 'y');
+            RET(ELEMENT_VALUE_TRI, 'i');
+        RET(ELEMENT_VALUE_QUATERNARY, 'y');
+            RET(ELEMENT_VALUE_TETRA, 'a');
+        RET(ELEMENT_VALUE_QUINARY, 'y');
+            RET(ELEMENT_VALUE_PENTA, 'a');
+        RET(ELEMENT_VALUE_SEXIMAL, 'l');
+            RET(ELEMENT_VALUE_HEXA, 'a');
+        RET(ELEMENT_VALUE_SEPTIMAL, 'l');
+            RET(ELEMENT_VALUE_HEPTA, 'a');
+        RET(ELEMENT_VALUE_OCTAL, 'l');
+            RET(ELEMENT_VALUE_OCTO, 'o');
+        RET(ELEMENT_VALUE_NONARY, 'y');
+            RET(ELEMENT_VALUE_ENNA, 'a');
+        RET(ELEMENT_VALUE_DECIMAL, 'l');
+            RET(ELEMENT_VALUE_GESIMAL, 'l');
+        RET(ELEMENT_VALUE_ELEVENARY, 'y');
+            RET(ELEMENT_VALUE_LEVA, 'a');
+        RET(ELEMENT_VALUE_DOZENAL, 'l');
+            RET(ELEMENT_VALUE_DOZA, 'a');
+        RET(ELEMENT_VALUE_BAKERS_DOZENAL, 'l');
+            RET(ELEMENT_VALUE_KERS_DOZENAL, 'l');
         //
-        case ELEMENT_VALUE_HEX: return 'x';
-            case ELEMENT_VALUE_TESSER: return 'r';
-        case ELEMENT_VALUE_SUBOPTIMAL: return 'l';
-            case ELEMENT_VALUE_MAL: return 'l';
+        RET(ELEMENT_VALUE_HEX, 'x');
+            RET(ELEMENT_VALUE_TESSER, 'r');
+        RET(ELEMENT_VALUE_SUBOPTIMAL, 'l');
+            RET(ELEMENT_VALUE_MAL, 'l');
         //
-        case ELEMENT_VALUE_VIGESIMAL: return 'l';
-            case ELEMENT_VALUE_ICOSI: return 'i';
+        RET(ELEMENT_VALUE_VIGESIMAL, 'l');
+            RET(ELEMENT_VALUE_ICOSI, 'i');
         //
-        case ELEMENT_VALUE_NIFTIMAL: return 'l';
-            case ELEMENT_VALUE_FETA: return 'a';
+        RET(ELEMENT_VALUE_NIFTIMAL, 'l');
+            RET(ELEMENT_VALUE_FETA, 'a');
         //
-        case ELEMENT_VALUE_CENTESIMAL: return 'l';
-            case ELEMENT_VALUE_HECTO: return 'o';
+        RET(ELEMENT_VALUE_CENTESIMAL, 'l');
+            RET(ELEMENT_VALUE_HECTO, 'o');
         //
-        case ELEMENT_VALUE_NARY: return 'y';
-            case ELEMENT_VALUE_ARY: return 'y';
-        case ELEMENT_VALUE_IMAL: return 'l';
-            case ELEMENT_VALUE_AL: return 'l';
+        RET(ELEMENT_VALUE_NARY, 'y');
+            RET(ELEMENT_VALUE_ARY, 'y');
+        RET(ELEMENT_VALUE_IMAL, 'l');
+            RET(ELEMENT_VALUE_AL, 'l');
         //
-        default:
-            return '\0';
+        default: return '\0';
     }
 
     return '\0';
@@ -179,67 +181,67 @@ char element_final_char(enum ELEMENT_VALUE ev) {
 
 char element_begin_char(enum ELEMENT_VALUE ev) {
     switch (ev) {
-        case ELEMENT_VALUE_INVALID: return 'i';
+        RET(ELEMENT_VALUE_INVALID, 'i');
         //
-        case ELEMENT_VALUE_NEGA: return 'n';
-        case ELEMENT_VALUE_VOT: return 'v';
+        RET(ELEMENT_VALUE_NEGA, 'n');
+        RET(ELEMENT_VALUE_VOT, 'v');
         //
-        case ELEMENT_VALUE_NULLARY: return 'n';
-        case ELEMENT_VALUE_UNARY: return 'u';
-            case ELEMENT_VALUE_UN: return 'u';
-            case ELEMENT_VALUE_HEN: return 'h';
-            case ELEMENT_VALUE_SNA: return 's';
-        case ELEMENT_VALUE_BINARY: return 'b';
-            case ELEMENT_VALUE_BI: return 'b';
-        case ELEMENT_VALUE_TRINARY: return 't';
-            case ELEMENT_VALUE_TRI: return 't';
-        case ELEMENT_VALUE_QUATERNARY: return 'q';
-            case ELEMENT_VALUE_TETRA: return 't';
-        case ELEMENT_VALUE_QUINARY: return 'q';
-            case ELEMENT_VALUE_PENTA: return 'p';
-        case ELEMENT_VALUE_SEXIMAL: return 's';
-            case ELEMENT_VALUE_HEXA: return 'h';
-        case ELEMENT_VALUE_SEPTIMAL: return 's';
-            case ELEMENT_VALUE_HEPTA: return 'h';
-        case ELEMENT_VALUE_OCTAL: return 'o';
-            case ELEMENT_VALUE_OCTO: return 'o';
-        case ELEMENT_VALUE_NONARY: return 'n';
-            case ELEMENT_VALUE_ENNA: return 'e';
-        case ELEMENT_VALUE_DECIMAL: return 'd';
-            case ELEMENT_VALUE_GESIMAL: return 'g';
-        case ELEMENT_VALUE_ELEVENARY: return 'e';
-            case ELEMENT_VALUE_LEVA: return 'l';
-        case ELEMENT_VALUE_DOZENAL: return 'd';
-            case ELEMENT_VALUE_DOZA: return 'd';
-        case ELEMENT_VALUE_BAKERS_DOZENAL: return 'b';
-            case ELEMENT_VALUE_KERS_DOZENAL: return 'k';
+        RET(ELEMENT_VALUE_NULLARY, 'n');
+        RET(ELEMENT_VALUE_UNARY, 'u');
+            RET(ELEMENT_VALUE_UN, 'u');
+            RET(ELEMENT_VALUE_HEN, 'h');
+            RET(ELEMENT_VALUE_SNA, 's');
+        RET(ELEMENT_VALUE_BINARY, 'b');
+            RET(ELEMENT_VALUE_BI, 'b');
+        RET(ELEMENT_VALUE_TRINARY, 't');
+            RET(ELEMENT_VALUE_TRI, 't');
+        RET(ELEMENT_VALUE_QUATERNARY, 'q');
+            RET(ELEMENT_VALUE_TETRA, 't');
+        RET(ELEMENT_VALUE_QUINARY, 'q');
+            RET(ELEMENT_VALUE_PENTA, 'p');
+        RET(ELEMENT_VALUE_SEXIMAL, 's');
+            RET(ELEMENT_VALUE_HEXA, 'h');
+        RET(ELEMENT_VALUE_SEPTIMAL, 's');
+            RET(ELEMENT_VALUE_HEPTA, 'h');
+        RET(ELEMENT_VALUE_OCTAL, 'o');
+            RET(ELEMENT_VALUE_OCTO, 'o');
+        RET(ELEMENT_VALUE_NONARY, 'n');
+            RET(ELEMENT_VALUE_ENNA, 'e');
+        RET(ELEMENT_VALUE_DECIMAL, 'd');
+            RET(ELEMENT_VALUE_GESIMAL, 'g');
+        RET(ELEMENT_VALUE_ELEVENARY, 'e');
+            RET(ELEMENT_VALUE_LEVA, 'l');
+        RET(ELEMENT_VALUE_DOZENAL, 'd');
+            RET(ELEMENT_VALUE_DOZA, 'd');
+        RET(ELEMENT_VALUE_BAKERS_DOZENAL, 'b');
+            RET(ELEMENT_VALUE_KERS_DOZENAL, 'k');
         //
-        case ELEMENT_VALUE_HEX: return 'h';
-            case ELEMENT_VALUE_TESSER: return 't';
-        case ELEMENT_VALUE_SUBOPTIMAL: return 's';
-            case ELEMENT_VALUE_MAL: return 'm';
+        RET(ELEMENT_VALUE_HEX, 'h');
+            RET(ELEMENT_VALUE_TESSER, 't');
+        RET(ELEMENT_VALUE_SUBOPTIMAL, 's');
+            RET(ELEMENT_VALUE_MAL, 'm');
         //
-        case ELEMENT_VALUE_VIGESIMAL: return 'v';
-            case ELEMENT_VALUE_ICOSI: return 'i';
+        RET(ELEMENT_VALUE_VIGESIMAL, 'v');
+            RET(ELEMENT_VALUE_ICOSI, 'i');
         //
-        case ELEMENT_VALUE_NIFTIMAL: return 'n';
-            case ELEMENT_VALUE_FETA: return 'f';
+        RET(ELEMENT_VALUE_NIFTIMAL, 'n');
+            RET(ELEMENT_VALUE_FETA, 'f');
         //
-        case ELEMENT_VALUE_CENTESIMAL: return 'c';
-            case ELEMENT_VALUE_HECTO: return 'h';
+        RET(ELEMENT_VALUE_CENTESIMAL, 'c');
+            RET(ELEMENT_VALUE_HECTO, 'h');
         //
-        case ELEMENT_VALUE_NARY: return 'n';
-            case ELEMENT_VALUE_ARY: return 'a';
-        case ELEMENT_VALUE_IMAL: return 'i';
-            case ELEMENT_VALUE_AL: return 'a';
+        RET(ELEMENT_VALUE_NARY, 'n');
+            RET(ELEMENT_VALUE_ARY, 'a');
+        RET(ELEMENT_VALUE_IMAL, 'i');
+            RET(ELEMENT_VALUE_AL, 'a');
         //
-        default:
-            return '\0';
+        default: return '\0';
     }
 
     return '\0';
 }
 
+#undef RET
 
 enum ELEMENT_VALUE pair_favor(enum ELEMENT_VALUE first, enum ELEMENT_VALUE last) {
     // io = io, ie = ie, ii = i, iu = i
