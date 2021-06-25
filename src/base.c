270ed38baab68e7e2a6ab0dbb4169cfec5e9163f
stkptr <85911392+stkptr@users.noreply.github.com>
2021-06-25 13:36:26 -0700

    Add help screen

--- src/base.c
+++ src/base.c
@@ -333,10 +333,29 @@ char *name(int n) {
 }
 
 
+void print_help(const char *program_name) {
+    printf(
+        "Usage: %s [base...]\n"
+        "\n"
+        "Convert decimal base numbers into jan Misali style base names\n",
+        program_name);
+}
+
+
 int main(int argc, char *argv[]) {
+    // help
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-' && argv[i][1] == 'h') {
+            print_help(argv[0]);
+            exit(0);
+        }
+    }
+
     for (int i = 1; i < argc; i++) {
         char *str = name(atoi(argv[i]));
         printf("%s\n", str);
         free(str);
     }
+
+    return 0;
 }
